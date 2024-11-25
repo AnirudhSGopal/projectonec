96686c92cda0beae8bde9699e74aa623878110e1
ANIRUDHSGOPAL028 <anirudhsgopal9609@gmail.com>
2024-11-25 14:25:59 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Page1 from './Components/pages/page1/page1';*/
 import FrontPage from './Components/FrontPage/FrontPage';
 
-
+import{HashRouter} from "react-router-dom";
 
 function App() {
   return (
    <div >
-  
+  <HashRouter>
    <FrontPage></FrontPage>
+   </HashRouter>
    </div>
   );
 }
