0012a06a09c971e8310a45d5ca2651646ba417b4
Anirudh S Gopal <anirudhsgopal554@gmail.com>
2024-11-25 17:45:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 /*import Navbar from './Components/Navbar/Navbar'
 import Page1 from './Components/pages/page1/page1';*/
 import FrontPage from './Components/FrontPage/FrontPage';
-
+import Page1 from './Components/pages/page1/page1';
+import Page2 from './Components/pages/page2/page2';
+import Page3 from './Components/pages/page3/page3';
+import Page4 from './Components/4page/page4';
+import Page5 from './Components/5page/page5';
+import Page6 from './Components/page6/page6';
+import Page7 from './Components/page7/page7';
+import Page8 from './Components/page8/page8';
 
 
 function App() {
@@ -10,6 +17,15 @@ function App() {
    <div >
   
    <FrontPage></FrontPage>
+   <Page1></Page1>
+    <Page2></Page2>
+    <Page3></Page3>
+    <Page4></Page4>
+    <Page5></Page5>
+    <Page6></Page6>
+    <Page7></Page7>
+    <Page8></Page8>
+
    </div>
   );
 }
